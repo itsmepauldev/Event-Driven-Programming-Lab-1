1bce9d8fffe2059eb94d3089ae7457fa091b22b4
Itspauldev <autorice65@gmail.com>
2024-09-03 23:12:16 +0800

    initial commit

---
+++ edp-Manglicmot/index.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('Click');
+const dclick = document.getElementById('dbclick');
+const down = document.getElementById('down');
+const up = document.getElementById('up');
+const over = document.getElementById('over');
+const out = document.getElementById('out');
+const move = document.getElementById('move');
+btn.onclick = function () {
+    alert('Click Done');
+}
+dclick.ondblclick = function () { 
+    alert('Double Click Done');
+}
+
+down.onmousedown = function () {
+    alert('Mouse Down Done');
+}
+up.onmouseup = function () {
+    alert('Mouse Up Done');
+}
+over.onmouseover = function () {
+    alert('Mouse Over');
+}
+out.onmouseout = function () {
+    alert('Bat mo inalis');
+}
+move.onmousemove = function () {
+    alert('Mouse Move');
+}
